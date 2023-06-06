af189494b671803eebe31fc289c8d427255cf9b4
Adryan Willian Machado Murmel <a2578867@campus.pg.utfpr.edu.br>
2023-06-06 17:17:42 -0300

    jogo da forca funcionando

---
+++ forca.py
@@ -0,0 +1,39 @@
+import random
+from re import T
+
+palavras = ["primeiro", "carlos", "jogar", "pizza", "hamburguer", "teclado"]
+
+palavra_escolhida = random.choice(palavras)
+
+print(palavra_escolhida)
+
+print(len(palavra_escolhida))
+
+display = []
+
+for n in range(len(palavra_escolhida)):
+    display.append("_")
+print(display)
+
+vidas = 6
+
+while vidas >= 0 and "_" in display:
+    chute = input("Digite uma letra: ")
+    print(chute)
+    cont = 0
+    acertou = False
+    for letra in palavra_escolhida:
+        if chute == letra:
+            display[cont] = chute
+            acertou = True
+        cont +=1
+    if not acertou:
+        vidas -= 1
+    print(display)
+    print(vidas)
+    if "_" not in display:
+        print("Você ganhou!!")
+        break
+    if vidas == 0:
+        print("Você perdeu!!\nA palavra era" + random.palavras)
+        break
